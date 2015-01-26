6ee246e1a3f691d7d0c45b19d0bf8c649d09561c
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2015-01-25 22:41:32 -0800

    got dat loop set up

--- templates/content-blog-home.php
+++ templates/content-blog-home.php
@@ -10,23 +10,32 @@ $args = array(
 );
 $query2 = new WP_Query( $args );
 
+
+
 // The 2nd Loop
-while ( $query2->have_posts() ) {
-	$query2->next_post();
-	?>
-	<article class="post">
-		<h3 class="title"><?php echo get_the_title( $query2->post->ID ) ?></h3>
-			<?php
-			$image_attributes = wp_get_attachment_image_src( get_post_thumbnail_id( $query2->post->ID ), 'large'); // returns an array
-			if( $image_attributes ) {
-				$img = wp_get_attachment_image_src( get_post_thumbnail_id( $query2->post->ID ), 'large' ); ?>
-				<img src="<?php echo $img[0] ?>" class="featured-image">
-			<?php } ?>
-		<?php echo get_post_field('post_content', $query2->post->ID); ?>
-		<?php // the_content(); ?>
-	</article>
-	<?php
-}
+if( $query2->have_posts() ):
+    while( $query2->have_posts() ): $query2->the_post();
+
+		// global $post;
+		// setup_postdata($post);
+
+		// $query2->next_post();
+		?>
+		<article class="post">
+			<h3 class="title"><?php echo get_the_title( $query2->post->ID ) ?></h3>
+				<?php
+				$image_attributes = wp_get_attachment_image_src( get_post_thumbnail_id( $query2->post->ID ), 'large'); // returns an array
+				if( $image_attributes ) {
+					$img = wp_get_attachment_image_src( get_post_thumbnail_id( $query2->post->ID ), 'large' ); ?>
+					<img src="<?php echo $img[0] ?>" class="featured-image">
+				<?php } ?>
+			<?php //echo get_post_field('post_content', $query2->post->ID); ?>
+			<?php the_content(); ?>
+		</article>
+		<?php
+		wp_reset_postdata();
+	endwhile;
+endif;
 
 // Restore original Post Data
 wp_reset_postdata();

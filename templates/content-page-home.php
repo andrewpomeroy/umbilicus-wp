0b9f579639876e82f9b60c5a2ef3342788cf8ca8
andrewpomeroy <andrewmichaelpomeroy@gmail.com>
2015-01-25 18:57:56 -0800

    added links and stuff

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -2,6 +2,12 @@
 <?php the_content(); ?>
 </div>
 
+<div class="pixel subtitles">
+	<h3><a href="http://globalgamejam.org/2015/games/umbilicus-%E2%80%93%C2%A0descent-team-delta" class="link--ggj">PLAY UMBILICUS</a></h3>
+</div>
+<hr>
+
+
 <?php // wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 		
 <?php 
